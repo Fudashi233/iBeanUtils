dfc6c151ac2bd57623d38823b0f3f6bd443ffc59
Fudashi233 <fudashi233@aliyun.com>
2018-08-12 17:09:24 +0800

    xxx

---
+++ src/main/java/cn/edu/jxau/beanutils/ConvertUtilsBean2.java
@@ -0,0 +1,11 @@
+package cn.edu.jxau.beanutils;
+
+/**
+ * Desc:
+ * ------------------------------------
+ * Author:fulei04@meituan.com
+ * Date:2018/8/12
+ * Time:下午5:09
+ */
+public class ConvertUtilsBean2 {
+}
